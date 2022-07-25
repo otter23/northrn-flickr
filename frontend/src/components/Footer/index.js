0560bdf51055b07ef87ec2d08d3a0eebd4eb9a39
Elan Katz <otter2388@gmail.com>
2022-07-25 12:15:22 -0700

    update footer to remove dead links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -18,7 +18,7 @@ export default function Footer() {
   const mainFooter = (
     <footer className='footer-container'>
       <div className='footer-top-row'>
-        <div>About</div>
+        {/* <div>About</div>
         <div>Jobs</div>
         <div>Blogs</div>
         <div>Developers</div>
@@ -34,12 +34,42 @@ export default function Footer() {
           <span className='material-symbols-outlined login-footer-icon'>
             expand_more
           </span>
-        </div>
+        </div> */}
+        <div>Tech Used:</div>
+        <div>Javascript</div>
+        <div>HTML5</div>
+        <div>CSS3</div>
+        <div>React</div>
+        <div>Redux</div>
+        <div>Express</div>
+        <div>Sequelize</div>
+        <div>PostgreSQL</div>
+        <div>AWS</div>
       </div>
       <div className='footer-bot-row'>
-        <div className='footer-smug'>SmugMug+Flickr.</div>
+        {/* <div className='footer-smug'>SmugMug+Flickr.</div> */}
+        <div className='footer-developer-name'>
+          <span>
+            <span className='footer-float-left'>
+              Northrn Flickr is a full-stack application clone of&nbsp;
+            </span>
+            <span>
+              <a
+                className='footer-slack-link'
+                href='https://flickr.com/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                flickr.com
+              </a>
+            </span>
+            <span className='footer-float-right'>
+              &nbsp;developed by Elan Katz in 2022.
+            </span>
+          </span>
+        </div>
         <div className='footer-icon-container'>
-          <img className='footer-fb-icon' src={fbIcon} alt='facebook icon' />
+          {/* <img className='footer-fb-icon' src={fbIcon} alt='facebook icon' />
           <img
             className='footer-twt-icon'
             src={twitterIcon}
@@ -50,7 +80,39 @@ export default function Footer() {
             className='footer-insta-icon'
             src={instaIcon}
             alt='instagram icon'
-          />
+          /> */}
+          <a
+            className='footer-github-link'
+            href='https://github.com/otter23'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {' '}
+          </a>
+          <a
+            className='footer-github-fork-link'
+            href='https://github.com/otter23/slackluster'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {' '}
+          </a>
+          <a
+            className='footer-linkedIn-link'
+            href='https://www.linkedin.com/in/elankatz/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {' '}
+          </a>
+          <a
+            className='footer-angellist-link'
+            href='https://angel.co/u/elankatz'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {' '}
+          </a>
         </div>
       </div>
     </footer>
